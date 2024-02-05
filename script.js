d2c02c34d61a2d2550a623a444c62c9cae6addfb
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-05 07:04:21 +0000

    added map

---
+++ script.js
@@ -0,0 +1,9 @@
+let singapore = [1.290270, 103.851959];
+
+const map = L.map('SingaporeMap').setView(singapore, 12);
+let marker = L.marker(singapore).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
