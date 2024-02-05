f9db3f51af5fcb0d6a7a6a4f6acd0de1f1fc721c
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-02-05 08:47:58 +0000

    debug

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let singapore = [1.290270, 103.851959];
 const map = L.map('SingaporeMap').setView(singapore, 12);
 let marker = L.marker(singapore).addTo(map);
 
+const markerCulster = L.markerCulsterGroup();
+marker.markerCulster.addTo(map);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+``
