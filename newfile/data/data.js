ec3a938799daa9b56b082de3fd500855ba039fbe
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-03-17 20:31:12 +0000

    added icon and instructions

---
+++ newfile/data/data.js
@@ -0,0 +1,32 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = "fsq3d3LjyCEbGabXxd80DhPXTZNZRAwY6MQ4ksMtawPdazQ="
+
+async function search(lat, lng, searchTerms) {
+    const response = await axios.get(`${BASE_API_URL}/places/search`, {
+        params: {
+            query: encodeURI(searchTerms), //encodeURI function to convert special characters to their encoded eqv so that query will be wellformed
+            ll: lat + "," + lng,
+            // categories:"13033",  // enable use of categories
+            sort: "DISTANCE",
+            radius: 5000,
+            limit: 50
+
+        },
+        headers: {
+            Accept: "application/json",
+            // Provide the API key here
+            Authorization: API_KEY
+        }
+    })
+    return response.data;
+}
+
+async function getPhotoFromFourSquare(fsqId) {
+    const response = await axios.get(`${BASE_API_URL}/places/${fsqId}/photos`, {
+        headers: {
+            Accept: "application/json",
+            Authorization: API_KEY
+        }
+    });
+    return response.data;
+}
--- newfile/script.js
+++ newfile/script.js
@@ -1,9 +1,5 @@
-
-
-
-
 let singapore = [1.290270, 103.851959];
-let map ;
+let map;
 let cities; // Ensure you have defined 'cities' somewhere in your code
 let circle = null;
 
@@ -11,22 +7,22 @@ document.addEventListener("DOMContentLoaded", async function () {
     // Initialize the map on the 'SingaporeMap' div
     map = L.map('SingaporeMap').setView(singapore, 12);
 
-    let singaporeMarker = L.marker(singapore,{ icon:singaporePin }).addTo(map);
+    let singaporeMarker = L.marker(singapore, { icon: singaporePin }).addTo(map);
     singaporeMarker.bindPopup("Welcome to Singapore! 🇸🇬").openPopup();
 
 
     map.on('zoomend', function () {
         if (map.getZoom() >= 15) {
             singaporeMarker.setOpacity(0); // Hide the marker when zoomed in beyond a certain level
-        } else if ( map.getZoom() < 10){
+        } else if (map.getZoom() < 10) {
             singaporeMarker.setOpacity(0);
         }
-        else{
+        else {
             singaporeMarker.setOpacity(1); // Show the marker when zoomed out
         }
     });
-    
-    let map1 =L.tileLayer('https://www.onemap.gov.sg/maps/tiles/Grey/{z}/{x}/{y}.png', {
+
+    let map1 = L.tileLayer('https://www.onemap.gov.sg/maps/tiles/Grey/{z}/{x}/{y}.png', {
         detectRetina: true,
         maxZoom: 19,
         minZoom: 11,
@@ -41,17 +37,17 @@ document.addEventListener("DOMContentLoaded", async function () {
         maxZoom: 19,
         attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
     });
-    var baseMaps = {
-        "map1":map1,
+    let baseMaps = {
+        "map1": map1,
         "OpenStreetMap": osm,
         "Food,Drink&taxi": osmHOT
     };
 
-   
+
     const response = await axios.get('data/Hotels.geojson');
     let markers = L.markerClusterGroup();
 
-    
+
 
     for (let feature of response.data.features) {
         // Create a new marker
@@ -69,56 +65,17 @@ document.addEventListener("DOMContentLoaded", async function () {
     };
     L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-    map.on('click', function(e) {
-        var clickedLat = e.latlng.lat;
-        var clickedLon = e.latlng.lng;
-      
-        // Example using Nominatim API (replace with your preferred service and API key)
-        var nominatimUrl = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + clickedLat + "&lon=" + clickedLon + "&addressdetails=1";
-      
-        fetch(nominatimUrl)
-          .then(response => response.json())
-          .then(data => {
-            if (data.address && data.address.road) {
-              var locationName = data.address.road;
-              L.popup("Location: " + locationName).setLatLng(e.latlng).openOn(map);
-            } else {
-              console.log("Location name not found");
-            }
-          })
-          .catch(error => console.error(error));
-      });
-
 })
 
+
 document.getElementById("searchbtn").addEventListener('click', async () => {
-    if (!circle || !map.hasLayer(circle)) {
-        let inputLocation = document.getElementById("searchin").value;
-        const apiUrl = `https://api.tomtom.com/search/2/geocode/${inputLocation}.json?key=56jDLQEJXuz69NIh1n9a6aukSHECQfsh`;
-        let userIlocation = await axios.get(apiUrl);
+    const searchTerms = document.querySelector("#searchin").value;
 
-        let newlocation = [userIlocation.data.results[0].position.lat, userIlocation.data.results[0].position.lon];
+    const centerPoint = map.getBounds().getCenter();
+    const data = await search(centerPoint.lat, centerPoint.lng, searchTerms);
 
-        // Fly to the new location
-        map.flyTo(newlocation, 14);
-
-        if (circle) {
-            map.removeLayer(circle);
-        }
-
-        console.log(newlocation);
-        circle = L.circle(newlocation, {
-            color: 'blue',
-            fillColor: '#F9BD06',
-            fillOpacity: 0.5,
-            radius: 500
-        }).addTo(map);
-    } else {
-        // If there is an existing circle, remove it
-        removeCircle();
-    }
+    addMarkersToMap(data, searchLayer, map);
 });
-
 
 function removeCircle() {
     if (circle) {
@@ -127,30 +84,72 @@ function removeCircle() {
     }
 }
 
-// https://api.mapbox.com/directions/v5/mapbox/driving/103.793022%2C1.404348%3B103.844353%2C1.278790?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=YOUR_MAPBOX_ACCESS_TOKEN')
-
-
-// `https://api.mapbox.com/directions/v5/mapbox/driving/${lon}%2C${lat}%3B${lon1}%2C${lat1}?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiYWttLTAwMSIsImEiOiJjbHFmY3kyMmIweHBxMnFxZjlnajZlbnBzIn0.q0Ajs20MNX5__KxsHzL4HQ`
-// https://api.mapbox.com/directions/v5/{profile}/{coordinates}
-
 document.querySelector("#btnDirection").addEventListener('click', () => {
-    axios.get('https://api.mapbox.com/directions/v5/mapbox/driving/103.793022%2C1.404348%3B103.844353%2C1.278790?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiYWttLTAwMSIsImEiOiJjbHFmY3kyMmIweHBxMnFxZjlnajZlbnBzIn0.q0Ajs20MNX5__KxsHzL4HQ')
-        .then(response => {
-            // Extract route geometry from API response
-            let data = response.data;
-            let route = L.geoJSON(data.routes[0].geometry, {
-                style: { color: 'blue' } // Customize route color
-            }).addTo(map);
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            axios.get(`https://api.mapbox.com/directions/v5/mapbox/cycling/${longitude},${latitude};103.846878,1.284390?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=pk.eyJ1IjoiYWttLTAwMSIsImEiOiJjbHFmY3kyMmIweHBxMnFxZjlnajZlbnBzIn0.q0Ajs20MNX5__KxsHzL4HQ`)
+                .then(response => {
+                    let data = response.data;
+                    let route = L.geoJSON(data.routes[0].geometry, {
+                        style: {
+                            color: 'blue',
+                            weight: 5,
+                            opacity: 0.6
+                        }
+                    }).addTo(map);
 
-            // Fit map to route bounds
-            map.fitBounds(route.getBounds());
-        })
-        .catch(error => {
-            console.error('Error fetching route:', error);
+                    map.fitBounds(route.getBounds());
+                    let navigationIcon = L.icon({
+                        iconUrl: './icons/Navigation.png', // Path to the PNG icon
+                        iconSize: [32, 32],
+                        iconAnchor: [16, 16]
+                    });
+
+                    L.marker([latitude, longitude], { icon: navigationIcon }).addTo(map)
+                        .bindPopup(`<lord-icon
+                        src="https://cdn.lordicon.com/oxbjzlrk.json"
+                        trigger="loop"
+                        delay="000"
+                        style="width:50px;height:50px">
+                    </lord-icon>
+                    <p>You are here.</p>`).openPopup();
+
+                    // Check if the instructions-container div already exists
+                    var existingContainer = document.getElementById('instructions-container');
+
+                    // If it exists, remove it
+                    if (existingContainer) {
+                        existingContainer.remove();
+                    }
+
+                    const instructionsContainer = document.createElement('div');
+                    instructionsContainer.id = "instructions-container"; // Optional: set an ID for easier selection
+
+                    const instructions = document.createElement('ol');
+
+                    data.routes[0].legs.forEach(leg => {
+                        leg.steps.forEach(step => {
+                            const li = document.createElement('li');
+                            li.textContent = step.maneuver.instruction;
+                            instructions.appendChild(li);
+                        });
+                    });
+
+                    instructionsContainer.appendChild(instructions);
+
+                    document.body.appendChild(instructionsContainer);
+
+
+                })
+                .catch(error => {
+                    console.error('Error fetching route:', error);
+                });
+        }, function (error) {
+            console.error("Error getting location:", error.message);
         });
-    // const centerpoint = map.getBounds().getCenter();
-    // console.log(centerpoint);
-})
-
-
-
+    } else {
+        console.error("Geolocation is not supported by your browser");
+    }
+});
